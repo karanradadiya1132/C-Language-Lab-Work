29fd420b5c8662630566b77695d55933d88cf850
karan radadiya <karanradadiya1132@gmail.com>
2025-07-19 14:14:11 +0530

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main ()
+{ 
+printf ("1.my name is karan radadiya\n");
+printf ("2.im 18 years old\n");
+printf ("3.right now im studing full satck devlopment/n")
+printf ("4.i live in surat city")
+}
